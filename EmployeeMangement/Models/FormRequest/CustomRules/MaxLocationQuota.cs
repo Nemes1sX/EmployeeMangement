1dfeefde4357e452e32e1b29336802d9529fcc89
Nemes1sX <kazstonk@gmail.com>
2022-03-01 14:27:55 +0200

    Clean code

--- EmployeeMangement/Models/FormRequest/CustomRules/MaxLocationQuota.cs
+++ EmployeeMangement/Models/FormRequest/CustomRules/MaxLocationQuota.cs
@@ -6,7 +6,7 @@ namespace EmployeeMangement.Models.FormRequest.CustomRules
 {
     public class MaxLocationQuota : ValidationAttribute
     {
-       private readonly EmployeeContext _db;
+        private readonly EmployeeContext _db;
 
         public MaxLocationQuota(EmployeeContext db)
         {
@@ -18,22 +18,17 @@ namespace EmployeeMangement.Models.FormRequest.CustomRules
             var employeeRequest = (EmployeeRequest)validationContext.ObjectInstance;
 
 
-            /*using (var db = new EmployeeContext())
-             {*/
+            var location = _db.Locations.Find(employeeRequest.LocationId);
 
-                 var location = _db.Locations.Find(employeeRequest.LocationId);
+            if (location == null)
+                return new ValidationResult("Location isn't existed");
 
-                 if (location == null)
-                     return new ValidationResult("Location isn't existed");
-
-                 var employeeCount = _db.Entry(location).Collection(e => e.Employees).Query().Count();
+            var employeeCount = _db.Entry(location).Collection(e => e.Employees).Query().Count();
 
 
-                 return employeeCount == location.MaxAllocation
-                     ? new ValidationResult("Max quota of employees in location is reached")
-                     : ValidationResult.Success;
-             /*}*/
-            //return base.IsValid(value, validationContext);
+            return employeeCount == location.MaxAllocation
+                ? new ValidationResult("Max quota of employees in location is reached")
+                : ValidationResult.Success;
         }
     }
 }
